343fabd57b7085c2bb22222d1f3711dc972b015d
hericlibong <hericlibong@gmail.com>
2024-11-10 21:10:53 +0100

    add unit test_security

--- epicevents/tests/test_security.py
+++ epicevents/tests/test_security.py
@@ -1,8 +1,7 @@
-from utils.security import hash_password, verify_password
+from epicevents.utils.security import hash_password, verify_password
 
 def test_hash_password():
     password = "password123"
     hashed_password = hash_password(password)
     assert hashed_password != password
     assert verify_password(password, hashed_password)
-    
